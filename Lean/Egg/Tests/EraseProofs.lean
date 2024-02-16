b4e04367b20cdbc49e4f5e7eceea27c8bf9290eb
Marcus Rossel <marcusrossel@icloud.com>
2024-02-16 14:29:10 +0100

    Remove PowerCalc tests

--- Lean/Egg/Tests/EraseProofs.lean
+++ Lean/Egg/Tests/EraseProofs.lean
@@ -1,12 +1,15 @@
 import Egg
 
 -- Tests for manually inspecting what terms look like with or without proof erasure.
-set_option trace.egg true
+
+example (i : Nat) (h : i < 10) : (Fin.mk i h).val = i := by
+  have : ∀ n m (g : n < m), (Fin.mk n g).val = n := by simp
+  egg (config := { eraseProofs := false }) [this]
 
 example (i : Nat) (h : i < 10) : (Fin.mk i h).val = i := by
   have : ∀ n m (g : n < m), (Fin.mk n g).val = n := by simp
   egg (config := { eraseProofs := true }) [this]
 
-example (i : Nat) (h : i < 10) : (Fin.mk i h).val = i := by
+example (i : Nat) (h : ∀ i : Nat, i < 10) : (Fin.mk i (h i)).val = i := by
   have : ∀ n m (g : n < m), (Fin.mk n g).val = n := by simp
-  egg (config := { eraseProofs := false }) [this]
+  egg (config := { eraseProofs := true }) [this]
