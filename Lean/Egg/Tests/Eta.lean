24b5c02cfc498dbb6eea8bb5b9e24c82d6943df3
Marcus Rossel <marcusrossel@icloud.com>
2024-03-05 15:52:17 +0100

    Add to eta-reduction test cases

--- Lean/Egg/Tests/Eta.lean
+++ Lean/Egg/Tests/Eta.lean
@@ -13,19 +13,28 @@ example : id (fun x => Nat.succ x) = id Nat.succ := by
 example : (fun x => Nat.succ x) x = Nat.succ x := by
   egg (config := { genEtaRw := true })
 
-example : (fun x => (fun y => Nat.succ y) x) = Nat.succ := by
+elab "eta" n:num fn:ident ty:term : term => open Lean.Elab.Term in do
+  let rec go (n : Nat) :=
+    if n = 0
+    then elabTerm fn none
+    else return .lam `x (← elabTerm ty none) (.app (← go <| n - 1) (.bvar 0)) .default
+  go n.getNat
+
+example : (eta 2 Nat.succ Nat) = Nat.succ := by
   egg (config := { genEtaRw := true })
 
-example : (fun x => (fun x => (fun x => Nat.succ x) x) x) = Nat.succ := by
+example : (eta 2 Nat.succ Nat) x = Nat.succ x := by
   egg (config := { genEtaRw := true })
 
-example : (fun x => (fun y => Nat.succ y) x) x = Nat.succ x := by
+example : id (eta 2 Nat.succ Nat) = id Nat.succ := by
   egg (config := { genEtaRw := true })
 
-example : (fun x => (fun x => (fun x => (fun x => Nat.succ x) x) x) x) = Nat.succ := by
+example : (eta 10 Nat.succ Nat) = Nat.succ := by
   egg (config := { genEtaRw := true })
 
-example : id (fun x => (fun y => Nat.succ y) x) = id Nat.succ := by
-  egg (config := { genEtaRw := true })
+example (a : Nat) (h : ∀ b : Nat, b.succ.add a = 0) : (10 |> fun x => Nat.succ x).add a = 0 := by
+  fail_if_success egg (config := { genEtaRw := false }) [h]
+  apply h
 
--- TODO: Construct a test case where the e-graph contains a cycle.
+example (a : Nat) (h : ∀ b : Nat, b.succ.add a = 0) : (10 |> fun x => Nat.succ x).add a = 0 := by
+  egg (config := { genEtaRw := true }) [h]
