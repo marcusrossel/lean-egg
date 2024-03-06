22adb7b37a623f22a72d28430499d33f577abad6
Marcus Rossel <marcusrossel@icloud.com>
2024-03-06 13:22:39 +0100

    Print errors on failure in test script when in CI-mode

--- Lean/Egg/Tests/run_tests.sh
+++ Lean/Egg/Tests/run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 tests_dir="$(realpath -s "$(dirname "$0")")"
-pkg_dir="${tests_dir}/../../.."
+pkg_dir="$tests_dir/../../.."
 module_prefix="Egg.Tests."
 skip_prefix="WIP"
 
@@ -27,25 +27,30 @@ for file in "$tests_dir"/*; do
                 continue
             fi
 
-            module_name="${module_prefix}${file_name}"
-
             if [[ "$ci_mode" == true ]]; then 
                 : 
             else
-                echo -n "Testing ${file_name} ..."
+                echo -n "Testing $file_name ..."
             fi
             
-            lake build $module_name >/dev/null 2>&1
+            module_name="$module_prefix$file_name"
+            output=$(lake build $module_name 2>&1)
 
             if [[ $? -eq 0 ]]; then
                 if grep -q "sorry" "$file"; then
-                    echo -e "\r🟡 ${file_name}            "
+                    echo -e "\r🟡 $file_name            "
                 else
-                    echo -e "\r✅ ${file_name}            "
+                    echo -e "\r✅ $file_name            "
                 fi
             else
 		        exit_code=1
-                echo -e "\r❌ ${file_name}           "
+                echo -e "\r❌ $file_name           "
+
+                if [[ "$ci_mode" == true ]]; then 
+                    echo "$output" | while IFS= read -r line; do
+                        echo "  ${line}"
+                    done
+                fi
 		    fi
         fi
     fi
