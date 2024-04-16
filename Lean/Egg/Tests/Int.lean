2b09600e26a312ac7f7a2941b1739217c968b9fd
Marcus Rossel <marcusrossel@icloud.com>
2024-04-16 18:11:06 +0200

    Undo sparse explanations introduced in 7ba9316ecfe007dbe12affaba4dec8d7ebb03f1d

--- Lean/Egg/Tests/Int.lean
+++ Lean/Egg/Tests/Int.lean
@@ -1,5 +1,5 @@
 import Egg
-import Std
+import Std.Data.Int.Lemmas
 
 example (a b c d : Int) : ((a * b) - (2 * c)) * d - (a * b) = (d - 1) * (a * b) - (2 * c * d) := by
   egg [Int.sub_mul, Int.sub_sub, Int.add_comm, Int.mul_comm, Int.one_mul]
