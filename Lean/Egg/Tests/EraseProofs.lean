376993ad453c0b3b97c08058337073493543010d
Marcus Rossel <marcusrossel@icloud.com>
2024-05-11 17:00:21 +0200

    Add failing test case for proof erasure

--- Lean/Egg/Tests/EraseProofs.lean
+++ Lean/Egg/Tests/EraseProofs.lean
@@ -17,6 +17,13 @@ example (i : Nat) (h : ∀ i : Nat, i < 10) : (Fin.mk i (h i)).val = i := by
   have : ∀ n m (g : n < m), (Fin.mk n g).val = n := by simp
   egg [this]
 
+-- BUG:
+example
+    (f : (a b : Nat) → a > b → Nat) (g : Nat → Nat) (a₁ a₂ b₁ b₂ c d : Nat) (h₁ : a₁ > b₁)
+    (h₂ : a₂ > b₂) (h₃ : a₁ = c) (h₄ : a₂ = c) (h₅ : b₁ = d) (h₆ : d = b₂) :
+    g (g (f a₁ b₁ h₁)) = g (g (f a₂ b₂ h₂)) := by
+  sorry -- egg [*]
+
 -- BUG: The rewrite is actually bidirectional, but the proof is the only reference to the mvar for
 --      `x` on the rhs.
 variable (h : ∀ x : Nat, x = (Exists.intro x x.zero_le).choose)
