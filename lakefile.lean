050bbb81577c311bbcc78542ec9f4c803f36d054
Marcus Rossel <marcusrossel@icloud.com>
2024-11-01 10:50:09 +0100

    Move egg backend directory

--- lakefile.lean
+++ lakefile.lean
@@ -23,9 +23,9 @@ extern_lib ffi pkg := do
   buildStaticLib libFile #[job]
 
 extern_lib egg_for_lean pkg := do
-  proc { cmd := "cargo", args := #["rustc", "--release", "--", "-C", "relocation-model=pic"], cwd := pkg.dir / "Rust" }
+  proc { cmd := "cargo", args := #["rustc", "--release", "--", "-C", "relocation-model=pic"], cwd := pkg.dir / "Rust" / "Egg" }
   let name := nameToStaticLib "egg_for_lean"
-  let srcPath := pkg.dir / "Rust" / "target" / "release" / name
+  let srcPath := pkg.dir / "Rust" / "Egg" / "target" / "release" / name
   IO.FS.createDirAll pkg.nativeLibDir
   let tgtPath := pkg.nativeLibDir / name
   IO.FS.writeBinFile tgtPath (← IO.FS.readBinFile srcPath)
