9b73a19ef63137763f1058a2898788170de38ddb
memoryleak47 <memoryleak47@protonmail.ch>
2025-01-30 15:11:22 +0100

    platform-independent build.rs

--- Rust/Egg/build.rs
+++ Rust/Egg/build.rs
@@ -1,6 +1,4 @@
 use std::process::Command;
-use std::ffi::OsStr;
-use std::os::unix::ffi::OsStrExt;
 
 extern crate cc;
 
@@ -8,7 +6,7 @@ fn main() {
     let mut cc = cc::Build::new();
     cc.file("../../C/rev_ffi.c");
     let bytes = Command::new("leanc").args(["--print-cflags"]).output().unwrap().stdout;
-    let s = OsStr::from_bytes(&bytes).to_str().unwrap();
+    let s = String::from_utf8(bytes).unwrap();
     for flag in s.split(" ") {
         let flag = flag.trim();
         cc.flag(flag);
