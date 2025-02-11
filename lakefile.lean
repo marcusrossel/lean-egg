d0f6a3a2b38420bb158c2d7ca5da35275ba2a83c
Marcus Rossel <marcusrossel@icloud.com>
2025-02-11 10:05:33 +0100

    Use `afterBuildCacheAsync` when building fetchable targets

--- lakefile.lean
+++ lakefile.lean
@@ -17,12 +17,13 @@ lean_lib Egg where
   -- This enables the interpreter to run functions marked `@[extern]`.
   precompileModules := true
 
-target importTarget pkg : System.FilePath := do
-  let oFile := pkg.buildDir / "c" / "ffi.o"
-  let srcJob ← inputTextFile <| pkg.dir / "C" / "ffi.c"
-  buildFileAfterDep oFile srcJob fun srcFile => do
-    let flags := #["-I", toString (← getLeanIncludeDir), "-fPIC"]
-    compileO oFile srcFile flags
+target importTarget pkg : System.FilePath :=
+  pkg.afterBuildCacheAsync do
+    let oFile := pkg.buildDir / "c" / "ffi.o"
+    let srcJob ← inputTextFile <| pkg.dir / "C" / "ffi.c"
+    buildFileAfterDep oFile srcJob fun srcFile => do
+      let flags := #["-I", toString (← getLeanIncludeDir), "-fPIC"]
+      compileO oFile srcFile flags
 
 extern_lib ffi pkg := do
   let job ← fetch <| pkg.target ``importTarget
@@ -30,30 +31,32 @@ extern_lib ffi pkg := do
   buildStaticLib libFile #[job]
 
 extern_lib egg_for_lean pkg := do
-  proc {
-    cmd := "cargo",
-    args := #["rustc", "--release", "--", "-C", "relocation-model=pic"],
-    cwd := pkg.dir / "Rust" / "Egg"
-  }
-  let name := nameToStaticLib "egg_for_lean"
-  let srcPath := pkg.dir / "Rust" / "Egg" / "target" / "release" / name
-  IO.FS.createDirAll pkg.nativeLibDir
-  let tgtPath := pkg.nativeLibDir / name
-  IO.FS.writeBinFile tgtPath (← IO.FS.readBinFile srcPath)
-  return pure tgtPath
+  pkg.afterBuildCacheAsync do
+    proc {
+      cmd := "cargo",
+      args := #["rustc", "--release", "--", "-C", "relocation-model=pic"],
+      cwd := pkg.dir / "Rust" / "Egg"
+    }
+    let name := nameToStaticLib "egg_for_lean"
+    let srcPath := pkg.dir / "Rust" / "Egg" / "target" / "release" / name
+    IO.FS.createDirAll pkg.nativeLibDir
+    let tgtPath := pkg.nativeLibDir / name
+    IO.FS.writeBinFile tgtPath (← IO.FS.readBinFile srcPath)
+    return pure tgtPath
 
 extern_lib slotted_for_lean pkg := do
-  proc {
-    cmd := "cargo",
-    args := #["rustc", "--release", "--", "-C", "relocation-model=pic"],
-    cwd := pkg.dir / "Rust" / "Slotted"
-  }
-  let name := nameToStaticLib "slotted_for_lean"
-  let srcPath := pkg.dir / "Rust" / "Slotted" / "target" / "release" / name
-  IO.FS.createDirAll pkg.nativeLibDir
-  let tgtPath := pkg.nativeLibDir / name
-  IO.FS.writeBinFile tgtPath (← IO.FS.readBinFile srcPath)
-  return pure tgtPath
+  pkg.afterBuildCacheAsync do
+    proc {
+      cmd := "cargo",
+      args := #["rustc", "--release", "--", "-C", "relocation-model=pic"],
+      cwd := pkg.dir / "Rust" / "Slotted"
+    }
+    let name := nameToStaticLib "slotted_for_lean"
+    let srcPath := pkg.dir / "Rust" / "Slotted" / "target" / "release" / name
+    IO.FS.createDirAll pkg.nativeLibDir
+    let tgtPath := pkg.nativeLibDir / name
+    IO.FS.writeBinFile tgtPath (← IO.FS.readBinFile srcPath)
+    return pure tgtPath
 
 @[test_driver]
 lean_exe TestDriver where
