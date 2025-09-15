3d786899e572b6abb9a41547c908947a7345e9d7
Marcus Rossel <marcusrossel@icloud.com>
2025-09-15 17:47:16 +0200

    Fix test case

--- Lean/Egg/Tests/Freshman Pow.lean
+++ Lean/Egg/Tests/Freshman Pow.lean
@@ -38,6 +38,7 @@ theorem freshmans_dream₂' : (x + y) ^ 2 = x ^ 2 + y ^ 2 := by
   char_two_ring
 
 set_option egg.timeLimit 10 in
+set_option egg.explLengthLimit 300 in
 theorem freshmans_dream₃ : (x + y) ^ 3 = x ^ 3 + x * y ^ 2 + x ^ 2 * y + y ^ 3 := by
   calc (x + y) ^ 3
     _ = (x + y) * (x + y) * (x + y)                     := by char_two_ring
