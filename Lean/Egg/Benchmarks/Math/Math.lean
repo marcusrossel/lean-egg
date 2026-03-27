b4b057b106b45781114541661f8eb06c7fde13ea
Marcus Rossel <marcusrossel@icloud.com>
2026-03-27 12:12:59 +0100

    Add script for running benchmarks

--- Lean/Egg/Benchmarks/Math/Math.lean
+++ Lean/Egg/Benchmarks/Math/Math.lean
@@ -3,6 +3,4 @@ import Math.Mathlib.Algebra.Group.Defs
 import Math.Mathlib.Algebra.Lie.Basic
 import Math.Mathlib.Order.BooleanAlgebra
 import Math.Zulip.AC
-import Math.Cast
-import Math.PushNeg
 import Math.Rotman
